f211876ecc2cf8d23f4d1edfd412ff789fef0aea
Monarul Islam <monarul007@gmail>
2025-11-18 15:26:15 +0600

    Fix: rename settings table name to avoid conflicts

--- database/migrations/2024_01_01_000000_create_settings_table.php
+++ database/migrations/2024_01_01_000000_create_settings_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('settings', function (Blueprint $table) {
+        Schema::create('module_settings', function (Blueprint $table) {
             $table->id();
             $table->string('key')->unique();
             $table->text('value');
--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    protected $table = 'module_settings';
     protected $fillable = [
         'key',
         'value',
